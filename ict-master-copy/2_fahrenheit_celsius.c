91390b8a33abe1681617438feedefd477633c449
Muhammad Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2024-09-01 12:40:37 +0600

    Delete ict-master-copy directory

--- ict-master-copy/2_fahrenheit_celsius.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    float temp;
-    printf("Enter Fahrenheit Value \n");
-    scanf("%f",&temp);
-    float celsius=(temp-32)/1.8;
-    printf("%f",celsius);
-    return 0;
-}
